97e2fc3dac73480193906932ed2bc39d7e7483db
kke11 <129268057+kke11@users.noreply.github.com>
2024-02-15 19:33:47 +0800

    Delete src directory

--- src/main/java/com/example/unsafedrive/mapper/VehicleOwnerMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.unsafedrive.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.unsafedrive.entity.pojos.VehicleOwner;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface VehicleOwnerMapper extends BaseMapper<VehicleOwner> {
-}
